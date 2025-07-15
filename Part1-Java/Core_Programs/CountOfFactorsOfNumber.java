06cd817f34f3c2d4da84d7411c21c4ceefb712b1
Rakshan shetty k <rakshanshetty16@gmail.com>
2025-07-15 12:35:23 +0530

    added core programs

---
+++ Part1-Java/Core_Programs/CountOfFactorsOfNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+ class CountOfFactorsOfNumber {
+     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num=s.nextInt();
+        int count=0;
+            System.out.print("Factors of Numbers are:");
+        for(int i=1;i<=num;i++){
+            if(num%i==0){//We can skip the "{}" when we have only one line in the if block of code
+            System.out.print(" "+i+" ");
+            count++;    
+        } 
+    } 
+        System.out.println("");
+        System.out.println("Count of Factors is "+count);  
+    }  
+    
+    }
+
