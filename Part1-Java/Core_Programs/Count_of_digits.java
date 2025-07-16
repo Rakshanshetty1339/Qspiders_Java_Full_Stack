5f05d3aad79f56288d2b73eb581bdc67dbd146df
Rakshan shetty k <rakshanshetty16@gmail.com>
2025-07-16 12:08:47 +0530

    updated

--- Part1-Java/Core_Programs/Count_of_digits.java
+++ Part1-Java/Core_Programs/Count_of_digits.java
@@ -6,12 +6,15 @@ public class Count_of_digits {
         System.out.println("Enter the number");
         int n=s.nextInt();
         int count=0;
-        while(n>0)
-        {
-            n=n/10;
+        // while(n>0){
+        //     n=n/10;
+        //     count++;
+        //     }
+        //     System.out.println("The number of digits in the number is "+count);
+        for(int i=n;i>0;i=i/10){
             count++;
-            }
-            System.out.println("The number of digits in the number is "+count);
+        }
+        System.out.println("The number of digits in the number is "+count);
             }
     }
 
