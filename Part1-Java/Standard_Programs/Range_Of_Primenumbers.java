331c4f0a0d430616e784d91bacae54f0e893b41a
Rakshan shetty k <rakshanshetty16@gmail.com>
2025-07-22 15:29:24 +0530

    updated

---
+++ Part1-Java/Standard_Programs/Range_Of_Primenumbers.java
@@ -0,0 +1,21 @@
+class Range_Of_Primenumbers
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in):
+		System.out.println("Enter the range");
+		int start=s.nextInt();
+		int ens=s.nextInt();
+		for(int=start;i<=end;i++){
+			System.out.println("Enter the number");
+			int num=s.nextInt();
+			int count=0;
+			for(int j=1;j<=num;j++){
+				if(i%j!==0)
+					count++;
+			}
+			
+		}
+				
+	}
+}
