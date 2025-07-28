2c3374b1910734c6263707acbc9ebcbb387c7644
Rakshan shetty k <rakshanshetty16@gmail.com>
2025-07-28 16:00:28 +0530

    added java standard programs

---
+++ Part1-Java/Standard_Programs/MagicNumber.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class  MagicNumber{
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=s.nextInt();
+	}
+	
+	public static int sumofdigits(int num){
+		int sum=0;
+		while(num>0){
+			int sd=num%10;
+			sum+=sd;
+		}
+		num
+			
+			
+			return sum;
+
+			{
+		
+}
